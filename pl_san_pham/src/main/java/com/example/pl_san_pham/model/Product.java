4a84e0fd374f976820f71078cd0c7aadbd08265c
Your Name <you@example.com>
2025-06-10 15:12:09 +0700

    Convert submodule to normal folder

--- pl_san_pham/src/main/java/com/example/pl_san_pham/model/Product.java
+++ pl_san_pham/src/main/java/com/example/pl_san_pham/model/Product.java
@@ -1,10 +1,21 @@
 package com.example.pl_san_pham.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+    @Column(name = "name")
     private String name;
+    @Column (name = "price")
     private int price;
+    @Column (name = "quantity")
     private int quantity;
+
     public Product(int id, String name, int price) {
         this.id = id;
         this.name = name;
--- pl_san_pham/src/main/java/com/example/pl_san_pham/service/ProductService.java
+++ pl_san_pham/src/main/java/com/example/pl_san_pham/service/ProductService.java
@@ -1,52 +1,37 @@
 package com.example.pl_san_pham.service;
 
 import com.example.pl_san_pham.model.Product;
+import com.example.pl_san_pham.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 @Service
 public class ProductService implements IProductService {
-    private static final List<Product> products = new ArrayList<>();
-
-    static {
-        products.add(new Product(1, "Iphone 15 Pro Max", 32990000));
-        products.add(new Product(2, "Samsung Galaxy S24", 25990000));
-        products.add(new Product(3, "Macbook Air M2", 29990000));
-    }
-
+@Autowired
+    private IProductRepository productRepository;
     @Override
     public List<Product> getAllProducts() {
-        return new ArrayList<>(products); // Trả về bản sao để tránh bị sửa từ bên ngoài
+        return productRepository.getAllProducts();
     }
 
     @Override
     public void addProduct(Product product) {
-        products.add(product);
+        productRepository.addProduct(product);
     }
 
     @Override
     public void deleteProduct(int id) {
-        products.removeIf(product -> product.getId() == id);
+        productRepository.deleteProduct(id);
     }
 
     @Override
     public void updateProduct(int id, Product product) {
-        for (int i = 0; i < products.size(); i++) {
-            if (products.get(i).getId() == id) {
-                products.set(i, product);
-                return;
-            }
-        }
+        productRepository.updateProduct(id, product);
     }
 
     @Override
     public Product getProduct(int id) {
-        for (Product p : products) {
-            if (p.getId() == id) {
-                return p;
-            }
-        }
-        return null;
+        return productRepository.getProduct(id);
     }
 }
